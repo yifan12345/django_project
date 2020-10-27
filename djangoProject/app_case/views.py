28ab1a12bbba84ffb708e8d8ed3679c83440d1e1
yifan <yifan1563@163.com>
2020-10-27 17:46:27 +0800

    Default Changelist

--- djangoProject/app_case/views.py
+++ djangoProject/app_case/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+import requests
+import json
 
 
 # Create your views here.
@@ -7,10 +9,30 @@ def list_case(request):
     return render(request, "case/debug.html")
 
 
+# 发送请求
 def send_req(request):
     if request.method == "GET":
-        id = request.GET.get("id")
-        name = request.GET.get("name")
-        return JsonResponse({
-            "status": 10200, "message": "success"
-        })
+        url = request.GET.get("url", "")
+        method = request.GET.get("method", "")
+        header = request.GET.get("header", "")
+        per_type = request.GET.get("per_type", "")
+        per_value = request.GET.get("per_value", "")
+        # try:
+        #     header = json.loads(header)
+        # except json.decoder.JSONDecodeError:
+        #     return JsonResponse({"status": 10101, "message": "header格式错误，必须是标准的json格式"})
+
+        try:
+            per_value = json.loads(per_value)
+        except json.decoder.JSONDecodeError:
+            return JsonResponse({"status": 10101, "message": "参数错误，必须是标准的json格式"})
+
+        if method == "get":
+            r = requests.get(url=url, params=per_value, headers=header)
+        if method == "post":
+            if per_type == "form":
+                r = requests.post(url=url, data=per_value, headers=header)
+            if per_type == "json":
+                r = requests.post(url=url, json=per_value, headers=header)
+
+    return JsonResponse({"status": 10200, "message": "success", "data": r.text})
